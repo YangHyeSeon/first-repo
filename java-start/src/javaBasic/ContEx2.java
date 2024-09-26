b0ae0ca1d142497494b7e754e16fc93586c5c039
7-ATE\yhs32 <hspob819b@gmail.com>
2024-09-27 00:36:26 +0900

    MyFirstGame
    2024.09.26
    
    JFrame, JLabel, JButton 사용 ==> 창, 라벨, 버튼 생성
    .setBorder 사용 ==> 버튼 테두리 생성
    addActionListener(new ActionListen() {} 사용 ==> Listener는 입력 계속 기다리는 중인 명령어, 중단할 때까지 계속 반복됨
                                                                            ==> 버튼 누르면 다른 창 띄움
                                                                            ==> 안에서 addActionListener(new ActionListen() {} 중첩해서 사용 가능
    if문 안에서 if 조건문 만족하는 횟수 카운트 하기 ==> 변수 선언하고 증감 연산자 사용

---
+++ java-start/src/javaBasic/ContEx2.java
@@ -0,0 +1,60 @@
+package javaBasic;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+    public class ContEx2 {
+
+        public static void main(String[] args) {
+            JFrame frame = new JFrame("Number Game");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(500, 200);
+            frame.setLayout(new GridLayout (4,1));
+
+            JLabel label = new JLabel("게임을 계속 하시겠습니까?");
+            label.setHorizontalAlignment(JLabel.CENTER);
+            Font font = new Font("돋음", Font.BOLD, 20);
+            frame.add(label);
+
+            JButton contin = new JButton("계속");
+            contin.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+            JButton exit = new JButton("종료");
+            exit.setBorder(BorderFactory.createLineBorder(Color.RED));
+            frame.add(contin);
+            frame.add(exit);
+
+            contin.addActionListener(new ActionListener() {
+                int gameCount = 0;
+                int countFirst = 0;
+                int countSecond = 0;
+                public void actionPerformed(ActionEvent e) {
+                    String firstInput = JOptionPane.showInputDialog("첫 번째 사람이 숫자를 입력하세요.");
+                    String secondInput = JOptionPane.showInputDialog("두 번째 사람이 숫자를 입력하세요.");
+
+                    int firstInput2 = Integer.parseInt(firstInput);
+                    int secondInput2 = Integer.parseInt(secondInput);
+
+                    if (firstInput2 > secondInput2) {
+                        JOptionPane.showMessageDialog(null, "첫 번째 사람이 이겼습니다!");
+                        countFirst++;
+                    } else {
+                        JOptionPane.showMessageDialog(null, "두 번째 사람이 이겼습니다!");
+                        countSecond++;
+                    }
+                    gameCount++;
+
+                    exit.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent e) {
+                            JOptionPane.showMessageDialog(null, "총 게임 수: " + gameCount + "\n첫 번째 사람이 이긴 수: " + countFirst + "\n두 번째 사람이 이긴 수: " + countSecond);
+
+                        }
+                    });
+                }
+
+            });
+
+            frame.setVisible(true);
+        }
+    }
