27428c4d7221940beae436499d689e844c95ef5a
7-ATE\yhs32 <hspob819b@gmail.com>
2024-09-27 01:14:22 +0900

    myFinalButton
    2024.09.27
    
    private static int var = 0; ==> 추가 공부 필요
    JFrame, JLabel, JButton 사용
    .setLayout(new GridLayout(row, colm) ==> 창 안에 라벨, 버튼 정렬 가능해짐
    addActionListener(new ActionListener) {
    public void actionPerformed(ActionEvent e) {} }); 사용

--- java-start/src/javaBasic/ContEx1.java
+++ java-start/src/javaBasic/ContEx1.java
@@ -1,39 +1,74 @@
 package javaBasic;
 
 import javax.swing.*;
-import javax.swing.text.StyledEditorKit;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ContEx1 {
+    private static int countIu = 0;
+    private static int countFro = 0;
+    private static int countStac = 0;
 
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("Popular Song Voting");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
+        frame.setSize(400, 400);
+        frame.setLayout(new GridLayout (5,1));
 
         JLabel label = new JLabel("Select your favorite song:");
         label.setHorizontalAlignment(JLabel.CENTER);
-        label.setFont(new Font("Gulim", Font.BOLD, 18));
+        label.setFont(new Font("돋음", Font.BOLD, 14));
         frame.add(label);
 
-        JButton btn1 = new JButton("아이유 - LILAC");
-        btn1.setSize(400, 50);
-        frame.add(btn1);
+        JButton btnIu = new JButton("아이유 - LILAC");
+        btnIu.setBackground(Color.white);
+        btnIu.setSize(400, 30);
+        frame.add(btnIu);
 
-        JButton btn2 = new JButton("프로미스나인 - We Go");
-        frame.add(btn2);
-        btn2.setSize(400, 50);
+        JButton btnFro = new JButton("프로미스나인 - We Go");
+        btnFro.setBackground(Color.white);
+        btnFro.setSize(400, 30);
+        frame.add(btnFro);
 
-        JButton btn3 = new JButton("프로미스나인 - We Go");
-        frame.add(btn3);
-        btn3.setSize(400, 50);
+        JButton btnStac = new JButton("스테이씨 - ASAP");
+        btnStac.setBackground(Color.white);
+        btnStac.setSize(400, 30);
+        frame.add(btnStac);
 
+        JButton btnExit = new JButton("종료");
+        btnExit.setBackground(Color.white);
+        btnExit.setSize(400, 30);
+        frame.add(btnExit);
 
+        btnIu.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"You voted for 아이유 - LILAC");
+                countIu++;
+            }
+        });
 
+        btnFro.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"You voted for 프로미스나인 - We Go");
+                countFro++;
+            }
+        });
 
+        btnStac.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"You voted for 스테이씨 - ASAP");
+                countStac++;
+            }
+        });
+
+        btnExit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"투표 결과:\n아이유 - LILAC:" + countIu + "표" + "\n프로미스나인 - We Go:" + countFro + "표" +"\n스테이씨 - ASAP:" + countStac + "표");
+            }
+        });
+        
         frame.setVisible(true);
 
     }
