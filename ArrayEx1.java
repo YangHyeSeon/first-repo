c9c682ca6439dff32aa51304245cfe1803ba1bf6
Yanghyeseon <hspob819b@gmail.com>
2024-09-27 17:47:42 +0900

    ArrayPractice
    2024.09.27
    
    Random r = new Random(); 과 배열 함께 사용
    Array.sort(); ==> 오름차순 정렬
    for문 사용
    if문 사용하여 max, min값 구하기

---
+++ ArrayEx1.java
@@ -0,0 +1,37 @@
+package deepPractice;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArrayEx1 {
+
+    public static void main(String[] args) {
+        int[] num = new int[20];
+        Random r = new Random();
+        for (int i = 0; i < 20; i++) {
+            num[i] = r.nextInt(100);
+        }
+        for (int x : num) {
+        }
+        System.out.println("배열 프린트: " + Arrays.toString(num));
+
+        Arrays.sort(num);
+        System.out.println("오름차순 정렬 후 프린트: " + Arrays.toString(num));
+
+        int max = num[0];
+        for (int i = 1; i < num.length; i++) {
+            if (max < num[i]) {
+                max = num[i];
+            }
+        }
+        System.out.println("MAX: " + max);
+
+        int min = num[0];
+        for (int i = 1; i < num.length; i++) {
+            if (min > num[i]) {
+                min = num[i];
+            }
+        }
+        System.out.println("MIN: " + min);
+    }
+}
