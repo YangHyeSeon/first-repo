2208e2c4d435e3842847a9fc0ac7ecdd6ce3e0c6
Yanghyeseon <hspob819b@gmail.com>
2024-09-30 11:22:29 +0900

    inputAndArrayFin
    2024.09.30
    
    - 값을 입력받아 배열에 저장
    while문 밖에서 int[] scores =  null;로 배열 선언해서 초기화
    while문-if문 안에서 scores = new in [students];로 배열 생성
    int scoresInput으로 값 입력 받고 scores[i]에 scoresInput값 저장
    
    -배열에 저장된 값 꺼내기
    for문 사용

---
+++ ArrayEx3.java
@@ -0,0 +1,60 @@
+package deepPractice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayEx3 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int students = 0;
+        int [] scores = null;
+
+        while (true) {
+            System.out.println("----------------------------------------------------");
+            System.out.println("1.학생수 | 2.점수입력 | 3. 점수리스트 | 4. 분석 | 5. 종료");
+            System.out.println("----------------------------------------------------");
+            System.out.print("선택> ");
+            int choice = input.nextInt();
+
+            if (choice == 1) {
+                System.out.print("학생수> ");
+                students = input.nextInt();
+                scores = new int[students];
+            } else if (choice == 2) {
+                for (int i = 0; i < students; i++) {
+                    System.out.print("scores" + "[" + i + "]>");
+                    int scoresInput = input.nextInt();
+                    scores[i] = scoresInput;
+                }
+            } else if (choice == 3) {
+                for (int j = 0; j < scores.length; j++) {
+                    System.out.println("scores" + "[" + j + "]>" + scores[j]);
+                }
+            } else if (choice == 4) {
+                int max = scores[0];
+                for (int x = 0; x < scores.length; x++) {
+                    if (max < scores[x]) {
+                        max = scores[x];
+                    }
+                }
+                System.out.println("최고 점수: " + max);
+
+                int sum = 0;
+                for (int y : scores) {
+                    sum += y;
+                }
+                double average = sum / scores.length;
+                System.out.println("평균 점수: " + average);
+
+            } else if (choice == 5) {
+                System.out.println("프로그램 종료");
+                break;
+            }
+        }
+
+
+
+
+    }
+}
